ec493b271e0cb3161aa12d61057c4835d775364a
hari <hariaish88893443@gmail.com>
2026-06-02 19:59:16 +0530

    cartservice

---
+++ demo/src/main/java/com/example/demo/CartController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class CartController {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @PostMapping("/cart")
+    public CartEntity addToCart(
+            @RequestBody CartEntity cart) {
+
+        return cartRepository.save(cart);
+    }
+
+    @GetMapping("/cart")
+    public List<CartEntity> getCart() {
+
+        return cartRepository.findAll();
+    }
+
+    @DeleteMapping("/cart/{id}")
+    public void removeCartItem(
+            @PathVariable Long id) {
+
+        cartRepository.deleteById(id);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/CartEntity.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class CartEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private String name;
+
+    private Double price;
+
+    private String image;
+
+    private Integer quantity;
+}
