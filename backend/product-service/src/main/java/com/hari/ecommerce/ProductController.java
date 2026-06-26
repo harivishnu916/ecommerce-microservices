3c47792f1ced2693710221aa3537d49aa5db5c0a
Hasini <hasinikkvl@gmail.com>
2026-06-26 18:42:14 +0530

    Final project submission

---
+++ backend/product-service/src/main/java/com/hari/ecommerce/ProductController.java
@@ -0,0 +1,34 @@
+package com.hari.ecommerce;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public List<productentity> getProducts() {
+
+        return productService.getAllProducts();
+
+    }
+
+    @GetMapping("/products/{id}")
+    public productentity getProductById(
+            @PathVariable Long id) {
+
+        return productService.getProductById(id);
+
+    }
+
+}
